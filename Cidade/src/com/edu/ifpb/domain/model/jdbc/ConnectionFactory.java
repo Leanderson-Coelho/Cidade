8133ebf394a545fb010b19daeda578e825173634
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2018-11-29 16:04:51 -0300

    adicionando svg e funcao viewBOx

--- Cidade/src/com/edu/ifpb/domain/model/jdbc/ConnectionFactory.java
+++ Cidade/src/com/edu/ifpb/domain/model/jdbc/ConnectionFactory.java
@@ -12,8 +12,8 @@ public class ConnectionFactory {
 	public Connection getConnection() throws ClassNotFoundException, SQLException{
 		Class.forName("org.postgresql.Driver");
 		url = "jdbc:postgresql://localhost:5432/Brasil";
-		usuario="leanderson";
-		senha="08lEo!@#";
+		usuario="";
+		senha="";
 		
 		return DriverManager.getConnection(url, usuario, senha);
 	}
